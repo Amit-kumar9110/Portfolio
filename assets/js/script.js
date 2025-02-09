cfd5ac58806bde686260256b889d1315eeaab8f1
Amit Kumar <amit9110950294@gmail.com>
2025-02-09 16:51:23 +0530

    files added

---
+++ assets/js/script.js
@@ -0,0 +1,124 @@
+
+const roles = ["Web Developer", "Designer", "Freelancer"];
+let roleIndex = 0;
+let charIndex = 0;
+const roleElement = document.getElementById("role");
+
+function typeEffect() {
+    if (charIndex < roles[roleIndex].length) {
+        roleElement.textContent += roles[roleIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(typeEffect, 150);
+    } else {
+        setTimeout(eraseEffect, 1000);
+    }
+}
+
+function eraseEffect() {
+    if (charIndex > 0) {
+        roleElement.textContent = roles[roleIndex].substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(eraseEffect, 100);
+    } else {
+        roleIndex = (roleIndex + 1) % roles.length;
+        setTimeout(typeEffect, 500);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    setTimeout(typeEffect, 500);
+});
+
+// download resume
+document.getElementById("downloadBtn").addEventListener("click", function () {
+    const resumeUrl = "assets/images/Resume.pdf"; // Ensure the file path is correct
+
+    const a = document.createElement("a");
+    a.href = resumeUrl;
+    a.setAttribute("download", "Amit_Resume.pdf"); // Ensure correct attribute usage
+
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+});
+
+
+// Function to set the theme and update UI
+function setTheme(theme) {
+    document.body.setAttribute('data-bs-theme', theme);
+    localStorage.setItem('theme', theme);
+    var switchThemeBtn = document.getElementById('switchTheme');
+    if (switchThemeBtn) {
+        switchThemeBtn.innerHTML = theme === 'dark' ? '<i class="bi bi-sun-fill"></i>' : '<i class="bi bi-moon-stars-fill"></i>';
+    }
+    //console.log(`Switched to ${theme} theme`);
+}
+
+var currentTheme = localStorage.getItem('theme') || 'dark';
+setTheme(currentTheme);
+
+// Event listener for the switch theme button
+var switchThemeBtn = document.getElementById('switchTheme');
+if (switchThemeBtn) {
+    switchThemeBtn.addEventListener('click', () => {
+        currentTheme = currentTheme === 'dark' ? 'light' : 'dark';
+        setTheme(currentTheme);
+    });
+}
+
+//AOS Initiliaze
+AOS.init();
+
+// Fixed Header & back to top button on Scroll
+window.addEventListener('scroll', () => {
+    // fixed header
+    const header = document.getElementById('header');
+    if (window.scrollY > 30 && !header.classList.contains('fixed-top')) {
+        header.classList.add('fixed-top');
+        document.getElementById('offcanvasNavbar').classList.add('fixedHeaderNavbar');
+    } else if (window.scrollY <= 30 && header.classList.contains('fixed-top')) {
+        header.classList.remove('fixed-top');
+        document.getElementById('offcanvasNavbar').classList.remove('fixedHeaderNavbar');
+    }
+
+    //backtotop
+    const backToTopButton = document.getElementById("backToTopButton");
+    if (window.scrollY > 400 && backToTopButton.style.display === 'none') {
+        backToTopButton.style.display = 'block';
+    } else if (window.scrollY <= 400 && backToTopButton.style.display === 'block') {
+        backToTopButton.style.display = 'none';
+    }
+});
+
+
+//jumping to top function
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+// my certificates slider
+$(document).ready(function () {
+    $(".certificates-slider").owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        responsive: {
+            0: { items: 1 },
+            800: { items: 2 },
+            1000: { items: 3 }
+        }
+    });
+
+    // Manually stop on hover and resume on leave
+    $(".certificates-slider").on("mouseenter", function () {
+        owl.trigger("stop.owl.autoplay");
+    });
+
+    $(".certificates-slider").on("mouseleave", function () {
+        owl.trigger("play.owl.autoplay", [5000]);
+    });
+});
